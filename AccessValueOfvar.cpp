38259e4cdd45895de02c79a38fc7066643cd4670
nirmal jangid <jangidnirmal15@gmail.com>
2025-07-25 15:06:47 +0530

    Access value of variable using pointer

---
+++ AccessValueOfvar.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num =15;
+    int* ptr = &num;
+
+      cout<<"number : "<<num<<endl;  //15
+      cout<<"whit pointer :"<<*ptr;  //15
+}
