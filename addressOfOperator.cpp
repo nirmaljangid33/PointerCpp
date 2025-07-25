77628f905ce6facca64297f3678fe0797eef0123
nirmal jangid <jangidnirmal15@gmail.com>
2025-07-25 15:01:44 +0530

    Pointer initialization with address-of operator

---
+++ addressOfOperator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num =10;
+    int *ptr = &num;
+
+      cout<<"number : "<<num<<endl;  //10
+      cout<<"whit pointer :"<<*ptr;  //10
+}
